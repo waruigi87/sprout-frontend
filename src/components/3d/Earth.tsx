3d1a8699c09a3656d7587b8b315ab15b125af5ae
Akifumi Nakayama <akifuminakayama@AkifuminoMacBook-Air.local>
2025-12-22 19:10:55 +0900

    import React　を削除

--- src/components/3d/Earth.tsx
+++ src/components/3d/Earth.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import { useRef } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { OrbitControls, useGLTF, Environment } from '@react-three/drei';
 import * as THREE from 'three';
